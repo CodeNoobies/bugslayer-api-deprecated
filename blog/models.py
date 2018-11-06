324f38a184a5b6522f6f86c5d9554a77c9fa17c5
Oxyrus <Oxyrus@users.noreply.github.com>
2018-11-06 14:01:24 -0500

    Add API example

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    content = models.TextField(max_length=5000, null=False)
+    # Set null in case the user gets deleted, just assume it was posted by
+    # someone anonymous
+    author = models.ForeignKey(User, related_name='articles', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+
+    def __str__(self):
+        return self.title
