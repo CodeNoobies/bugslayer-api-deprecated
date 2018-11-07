c60da4c17c9248145be454d49449b6a4c9a57285
Oxyrus <Oxyrus@users.noreply.github.com>
2018-11-07 13:41:27 -0500

    Remove code smells

--- blog/tests.py
+++ blog/tests.py
@@ -4,15 +4,3 @@ from django.contrib.auth.models import User
 from .models import Article
 from .views import index
 
-"""
-class IndexTests(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(username='Jon', email='jon@doe.com', password='jondoe')
-        self.article = Article.objects.create(title='Test Article', content='Just a test article', author=self.user)
-        url = reverse('index')
-        self.response = self.client.get(url)
-
-    def test_home_url_resolves_home_view(self):
-        view = resolve('/')
-        self.assertEquals(view.func, index)
-"""
