9747d7305ec1fb124eecf976a8f17020a7d3ac9d
Oxyrus <Oxyrus@users.noreply.github.com>
2018-11-06 19:57:18 -0500

    Clean code

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from blog.views import ListArticlesView
 
 
 urlpatterns = [
-    path('articles/', ListArticlesView.as_view(), name='articles-all'),
+    path('', ListArticlesView.as_view(), name='articles-all'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
 from .models import Article
 from .serializers import ArticleSerializer
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 
-"""
-@csrf_exempt
-def article_list(request):
-    if request.method == 'GET':
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return JsonResponse(serializer.data, safe=False)
-"""
 
 class ListArticlesView(generics.ListAPIView):
     """
@@ -22,7 +10,3 @@ class ListArticlesView(generics.ListAPIView):
     """
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-
-
-# class GetArticleView(generics.)
-
